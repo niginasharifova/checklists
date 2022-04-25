83ebe1209cb5e98223575b4c8ab4cd2a41afe0cb
Nigina Sharifova <sharifova.n.7@gmail.com>
2022-04-25 17:30:15 +0500

    added cells to 3rd screen

---
+++ Checklists/Cells/DateTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DateTableViewCell.swift
+//  Checklists
+//
+//  Created by beehive-iOS on 4/25/22.
+//
+
+import UIKit
+
+class DateTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ Checklists/Controllers/AddItemTableViewController.swift
@@ -0,0 +1,47 @@
+//
+//  AddItemTableViewController.swift
+//  Checklists
+//
+//  Created by beehive-iOS on 4/22/22.
+//
+
+import UIKit
+
+class AddItemTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "TextFieldTableViewCell", for: indexPath) as! TextFieldTableViewCell
+            return cell
+        } else if indexPath.section == 1 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "SwitchTableViewCell", for: indexPath) as! SwitchTableViewCell
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "DateTableViewCell", for: indexPath) as! DateTableViewCell
+            return cell
+        }
+    }
+}
+
+
